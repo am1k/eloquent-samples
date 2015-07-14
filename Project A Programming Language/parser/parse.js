c6d303ba4f5301964b4951ad2f5feba314d7033c
am1k <vitaliy_bog@mail.ru>
2015-07-14 10:59:46 +0300

    add new test

--- Project A Programming Language/parser/parse.js
+++ Project A Programming Language/parser/parse.js
@@ -6,10 +6,25 @@ var parseExpression = require('./parseExpression'),
     skipSpace = require('./skipSpace');
 
 function parse(program) {
+    if (isArray(program) || isObject(program) || isNumber(program) || program === null || program === undefined) {
+        return false;
+    }
     var result = parseExpression(program);
     if (skipSpace(result.rest).length > 0)
         throw new SyntaxError("Unexpected text after program");
     return result.expr;
+}
+
+function isArray(obj) {
+    return Object.prototype.toString.call(obj).toUpperCase() === '[OBJECT ARRAY]';
+}
+
+function isObject(obj) {
+    return Object.prototype.toString.call(obj).toUpperCase() === '[OBJECT OBJECT]';
+}
+
+function isNumber(obj) {
+    return Object.prototype.toString.call(obj).toUpperCase() === '[OBJECT NUMBER]';
 }
 
 module.exports = parse;
--- Project A Programming Language/test.js
+++ Project A Programming Language/test.js
@@ -4,6 +4,8 @@
 
 var skipSpace= require('./parser/skipSpace'),
     parse = require('./parser/parse'),
+    evaluate = require('./evaluator/evaluator'),
+    topEnv = require('./environment/topEnv'),
     expect =  require('expect.js');
 
 describe('Skip', function() {
@@ -15,9 +17,52 @@ describe('Skip', function() {
         });
 
         describe('#parse()', function() {
-            it('first test', function() {
-                expect(parse("# hello\nx")).to.eql({ type: 'word', name: 'x' });
+            it('parse string to object', function() {
+                expect(parse("hello")).to.eql({ type: 'word', name: 'hello' });
             });
+
+            it('parse string to object', function() {
+                expect(parse(("+(a, 10)"))).to.eql(
+                    {type: "apply",
+                        operator: {type: "word", name: "+"},
+                        args: [{type: "word", name: "a"},
+                        {type: "value", value: 10}]})
+            });
+
+            it('check that string value', function() {
+                expect(parse('string')).to.be.a('object');
+            });
+
+            it('check when user enter null', function() {
+                expect(parse(null)).to.be.null;
+            });
+
+            it('check when user enter undefined', function() {
+                expect(parse(undefined)).to.be.undefined;
+            });
+
+            it('check when user enter number', function() {
+                expect(parse(12)).to.be.false;
+            });
+
+            it('check when user enter array', function() {
+                expect(parse([2,4])).to.be.false;
+            });
+
+            it('check when user enter object', function() {
+                expect(parse({1:[2,3]})).to.be.false;
+            });
+
         });
+
+        describe('#evaluate()', function() {
+            it('Should return false', function() {
+                var prog = parse("if(true, false, true)");
+                expect(evaluate(prog, topEnv)).to.equal(false);
+            });
+
+        });
+
+
 
 });
