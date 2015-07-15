237f8fe258147729a70e9ab1c82ecd3706b6a968
am1k <vitaliy_bog@mail.ru>
2015-07-15 16:07:25 +0300

    add new tasks

--- Project A Programming Language/test.js
+++ Project A Programming Language/test.js
@@ -6,7 +6,8 @@ var skipSpace= require('./parser/skipSpace'),
     parse = require('./parser/parse'),
     evaluate = require('./evaluator/evaluator'),
     topEnv = require('./environment/topEnv'),
-    expect =  require('expect.js');
+    expect =  require('expect.js'),
+    sinon = require('sinon');
 
 describe('Skip', function() {
 
@@ -60,8 +61,35 @@ describe('Skip', function() {
                 var prog = parse("if(true, false, true)");
                 expect(evaluate(prog, topEnv)).to.equal(false);
             });
-
         });
+
+    describe('#topEnv', function() {
+        it('show topEnv["print"]', function() {
+
+            var spy = sinon.spy();
+            topEnv.print(spy());
+            expect(spy.called).to.equal(true);
+        });
+
+        it('topEnv["array"] return array', function() {
+
+            var arr = [2, 1, 4, 5];
+            var arr2 = [1, 3, 5, 6];
+            expect(topEnv.array(arr, arr2)).to.be.an('array');
+        });
+
+        it('topEnv["length"] return length of array', function() {
+
+            var arr = [12, 13, 14, 15];
+            expect(topEnv.length(arr)).to.equal(arr.length);
+        });
+
+        it('topEnv["element"]return element of array', function() {
+
+            var arr = [1, 3, 4, 5];
+            expect(topEnv.element(arr, 2)).to.equal(arr[2]);
+        });
+    });
 
 
 
